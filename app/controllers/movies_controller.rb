65c2e75b8d2476d834e76d9c6c7e0f0d4f057fb7
Dmitry Kabanov <dkabanov6@gmail.com>
2022-06-19 12:16:58 +0500

    step 12-2: forms: editing records

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,10 +6,31 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    @movie = movie
   end
 
   def edit
-    @movie = Movie.find(params[:id])
+    @movie = movie
+  end
+
+  def update
+    movie.update(movie_params)
+    redirect_to movie
+  end
+
+  private
+
+  def movie
+    @movie ||= Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(
+      :title,
+      :rating,
+      :total_gross,
+      :description,
+      :released_on
+    )
   end
 end
