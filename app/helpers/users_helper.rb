49b4431ae50d0c41f770986faf82d605096478dd
Dmitry Kabanov <dkabanov6@gmail.com>
2022-07-10 07:56:17 +0500

    step 27: user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def profile_image(user)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
+    image_tag(gravatar_url, alt: user.name)
+  end
 end
