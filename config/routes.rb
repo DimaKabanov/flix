af39d6da25e9e894e0b06fbab5f2688f96639319
Dmitry Kabanov <dkabanov6@gmail.com>
2022-07-12 16:37:04 +0500

    step 29: sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :users
   get 'signup', to: 'users#new'
 
+  resource :session, only: %i[new create destroy]
+  get 'signin', to: 'sessions#new'
+
   resources :movies do
     resources :reviews
   end
