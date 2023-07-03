e97016c973e362ef5ac2aff7fa031b98c936c78c
Dmitry Kabanov <dkabanov6@gmail.com>
2023-07-03 20:58:07 +0500

    added rubocop

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ install:
 
 start:
 	bin/rails s -p 3000 -b "0.0.0.0"
+
+lint:
+	bundle exec rubocop
+
+linter-fix:
+	bundle exec rubocop -A
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MoviesController < ApplicationController
   def index
     @movies = ['Iron Man', 'Superman', 'Spider-Man', 'Batman']
