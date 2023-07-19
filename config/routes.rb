e2860b492941d8c25099b2b0b2f0a7decf1802c6
Dmitry Kabanov <dkabanov6@gmail.com>
2023-07-19 07:41:50 +0500

    step 11: routes: linking pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'movies#index'
   get 'movies', to: 'movies#index'
-  get 'movies/:id', to: 'movies#show'
+  get 'movies/:id', to: 'movies#show', as: 'movie'
 end
