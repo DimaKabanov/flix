386ed5d40c9bc0a9de9065d1ae334a887dd06cc2
Dmitry Kabanov <dkabanov6@gmail.com>
2023-07-25 07:13:29 +0500

    step 18: model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
+  RATINGS = %W(G PG PG-13 R NC-17)
+
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, inclusion: { in: RATINGS }
+
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: 'must be a JPG or PNG image'
+  }
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
