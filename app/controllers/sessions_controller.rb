bc77648325360cb89ad5c1304a36ca6077683a15
Dmitry Kabanov <dkabanov6@gmail.com>
2023-08-12 13:44:38 +0500

    step 30: authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,18 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by(email: params[:email]) ||
+           User.find_by(username: params[:username])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = 'Invalid email/password combination!'
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def destroy; end
 end
