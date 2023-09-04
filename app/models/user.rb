c5a38dcefe95507e639a4d00fe3eadba9caa87e2
Dmitry Kabanov <dkabanov6@gmail.com>
2023-09-04 07:38:23 +0500

    step 37: through associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_movies, through: :favorites, source: :movie
 
   validates :name, :username, :email, presence: true
 
