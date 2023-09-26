be2631a1d670a3d6a352626844eeb3fc75810df2
Dmitry Kabanov <dkabanov6@gmail.com>
2023-09-26 09:13:55 +0500

    step 42: active storage file

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -16,4 +16,11 @@ module MoviesHelper
   def nav_link_to(name, path)
     link_to name, path, class: current_page?(path) ? 'active' : nil
   end
+
+  def main_image(movie)
+    image_tag(
+      movie.main_image.attached? ? movie.main_image : 'placeholder.png',
+      alt: movie.title
+    )
+  end
 end
